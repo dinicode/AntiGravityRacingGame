e691f3233ac9375a34323c368e5eef890ddcdad7
Carldini Ritchi Oreste <130177388+dinicode@users.noreply.github.com>
2023-07-24 14:59:38 -0400

    Update Vehicle.java

--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class Vehicle {
+import java.util.Random;
+
+class Vehicle {
+    private String name;
+    private int speed;
+    private int acceleration;
+    private int handling;
+    private int lapsCompleted;
+
+    public Vehicle(String name, int speed, int acceleration, int handling) {
+        this.name = name;
+        this.speed = speed;
+        this.acceleration = acceleration;
+        this.handling = handling;
+        this.lapsCompleted = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getAcceleration() {
+        return acceleration;
+    }
+
+    public int getHandling() {
+        return handling;
+    }
+
+    public int getLapsCompleted() {
+        return lapsCompleted;
+    }
+
+    public void completeLap() {
+        lapsCompleted++;
+    }
+
+    public int getDistanceCovered(int time) {
+        Random random = new Random();
+        int randomFactor = random.nextInt(11) - 5; // Random number between -5 and 5
+        return (speed + randomFactor) * time + (acceleration + randomFactor) * time * time / 2;
+    }
 }
+
+
