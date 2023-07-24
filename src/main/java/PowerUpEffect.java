9f41007840b98700f0ccdcfe38acbff4cdbac93c
Carldini Ritchi Oreste <130177388+dinicode@users.noreply.github.com>
2023-07-24 15:01:39 -0400

    Update PowerUpEffect.java

--- src/main/java/PowerUpEffect.java
+++ src/main/java/PowerUpEffect.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class PowerUpEffect {
+public class PowerUpEffect {
+    private double speedBoostPercentage;
+    private boolean invincibility;
+    // Add more effect attributes as needed...
+
+    public PowerUpEffect(double speedBoostPercentage, boolean invincibility) {
+        this.speedBoostPercentage = speedBoostPercentage;
+        this.invincibility = invincibility;
+    }
+
+    public double getSpeedBoostPercentage() {
+        return speedBoostPercentage;
+    }
+
+    public boolean isInvincibility() {
+        return invincibility;
+    }
+
+    public void setSpeedBoostPercentage(double speedBoostPercentage) {
+        this.speedBoostPercentage = speedBoostPercentage;
+    }
+
+    public void setInvincibility(boolean invincibility) {
+        this.invincibility = invincibility;
+    }
 }
