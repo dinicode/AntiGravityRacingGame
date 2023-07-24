1153d1d0829039403858a65484b5f7ca1dc3352a
Carldini Ritchi Oreste <130177388+dinicode@users.noreply.github.com>
2023-07-24 15:02:29 -0400

    Update PowerUp.java

--- src/main/java/PowerUp.java
+++ src/main/java/PowerUp.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class PowerUp {
-}
+public class PowerUp {
+    private String name;
+    private int durationInSeconds;
+    private PowerUpEffect effect;
+
+    public PowerUp(String name, int durationInSeconds, PowerUpEffect effect) {
+        this.name = name;
+        this.durationInSeconds = durationInSeconds;
+        this.effect = effect;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDurationInSeconds() {
+        return durationInSeconds;
+    }
+
+    public PowerUpEffect getEffect() {
+        return effect;
+    }
+
+    PowerUp powerUp1 = new PowerUp("Turbo Boost", 5, new PowerUpEffect(0.2, false));
+    PowerUp powerUp2 = new PowerUp("Invincibility", 10, new PowerUpEffect(0, true));}
+
